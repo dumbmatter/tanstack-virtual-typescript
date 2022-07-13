95bf4f0086e726a9d6f00f51aa5b108ce2482279
Jeremy Scheff <jdscheff@gmail.com>
2022-07-13 13:59:21 -0400

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,53 @@
+import { useRef } from 'react';
+import { useVirtualizer } from '@tanstack/react-virtual';
+
+function App() {
+  // The scrollable element for your list
+  const parentRef = useRef<HTMLDivElement | null>(null)
+
+  // The virtualizer
+  const rowVirtualizer = useVirtualizer({
+    count: 10000,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 35,
+  })
+
+  return (
+    <>
+      {/* The scrollable element for your list */}
+      <div
+        ref={parentRef}
+        style={{
+          height: `400px`,
+          overflow: 'auto', // Make it scroll!
+        }}
+      >
+        {/* The large inner element to hold all of the items */}
+        <div
+          style={{
+            height: `${rowVirtualizer.getTotalSize()}px`,
+            width: '100%',
+            position: 'relative',
+          }}
+        >
+          {/* Only the visible items in the virtualizer, manually positioned to be in view */}
+          {rowVirtualizer.getVirtualItems().map((virtualItem: any) => (
+            <div
+              key={virtualItem.key}
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: `${virtualItem.size}px`,
+                transform: `translateY(${virtualItem.start}px)`,
+              }}
+            >
+              Row {virtualItem.index}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
